ccf08c82605a09b0ebabdb448cf48224b5722e39
Apple <apple@Apples-MBP-5.attlocal.net>
2019-07-16 14:38:33 -0400

    Complete Iteration 4

--- viewFinder/PhotoTableViewController.swift
+++ viewFinder/PhotoTableViewController.swift
@@ -42,7 +42,6 @@ class PhotoTableViewController: UITableViewController {
     
    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 10
         return photos.count
     }
        
@@ -64,27 +63,53 @@ class PhotoTableViewController: UITableViewController {
         
         return cell
     }
-   
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "moveToDetail", sender: photos[indexPath.row])
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "moveToDetail" {
+            
+            if let photoDetailView = segue.destination as? PhotoDetailViewController {
+                
+                if let photoToSend = sender as? Photos {
+                    
+                    photoDetailView.photo = photoToSend
+                }
+            }
+        }
+    }
+  
 
-    /*
+   
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+   
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            
+            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                
+                let photoToDelete = photos[indexPath.row]
+                
+                context.delete(photoToDelete)
+                
+                (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+                getPhotos()
+                
+            }
+           
+            
+        }
     }
-    */
+   
 
     /*
     // Override to support rearranging the table view.
